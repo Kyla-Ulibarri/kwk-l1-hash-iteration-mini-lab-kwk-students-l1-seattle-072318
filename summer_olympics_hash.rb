1b0758ce5af9ece7b2e9b53c5b5f5e86668c63c0
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-26 16:12:17 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -26,9 +26,9 @@ puts iterate_through_hash
  def iterate_through_keys
    iterate_through_hash.keys = city_names
    puts city_names
-   #city_names.each do |upcased_cities|
- # puts #{upcased_cities}
-#end 
+   city_names.each do |upcased_cities|
+  puts #{upcased_cities}
+end 
 end
 
 puts iterate_through_keys
