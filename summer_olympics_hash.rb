039a9c6b46cb055fce6a1aed7315f0893feac4a6
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-26 16:47:02 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -7,7 +7,7 @@ puts create_olympics_hash
 
 def add_a_key_value_pair
   summer_olympics_hash={
-    :Sydney => "2000",
+  :Sydney => "2000",
   :Athens => "2004",
   :Beijing => "2008",
   :London => "2012"
